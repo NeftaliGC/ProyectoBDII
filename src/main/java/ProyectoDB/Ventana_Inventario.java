83ed8a8f0a3eb33928c0460ffb2ce326e43058e8
cruzz9 <164835098+cruzz9@users.noreply.github.com>
2024-05-25 00:55:41 -0600

    Terminando: Ventana de Inventario

---
+++ src/main/java/ProyectoDB/Ventana_Inventario.java
@@ -0,0 +1,74 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+public class Ventana_Inventario {
+
+	public Ventana_Inventario(Connection connection) {
+        this.connection = connection;
+        initialize();
+    	}
+
+    public static void main(String[] args) {
+        JFrame ventana = new JFrame("Inventario");
+
+        ventana.setSize(500, 500);
+        
+        ventana.setLocationRelativeTo(null);
+        
+        JPanel panelBotones = new JPanel(new GridBagLayout());
+        panelBotones.setBackground(new Color(173, 216, 230));
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = GridBagConstraints.RELATIVE;
+        gbc.insets = new Insets(25, 25, 25, 25); 
+
+        JLabel tituloLabel = new JLabel("Inventario");
+	tituloLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        panelBotones.add(tituloLabel, gbc);
+
+        
+        String[] textosBotones = {"Dar de alta", "Dar de baja", "Cambios", "Consultas", "Reportes(Datamart)"};
+
+   
+        for (int i = 0; i < 5; i++) {
+            JButton boton = new JButton(textosBotones[i]);
+            panelBotones.add(boton, gbc);
+
+            
+            boton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String textoBoton = ((JButton) e.getSource()).getText();
+                    System.out.println(textoBoton);
+                    switch (textoBoton) {
+                        case "Dar de alta":
+                            //new ();
+                            break;
+			case "Dar de baja":
+			//new();
+                       	break;
+			case "Cambios":
+			//new();
+			break;
+			case "Consultas":
+			//new();
+			break;
+			case "Reportes(Datamart)":
+			//new();
+			break;
+                    }
+                }
+            });
+        }
+        
+        ventana.add(panelBotones);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
+    }
+}
+
+
+
