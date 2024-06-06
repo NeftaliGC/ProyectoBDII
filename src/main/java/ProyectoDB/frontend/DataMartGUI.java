2ad69bd9358ae0d561cfa4b6a0d0d2fca4cba852
smtzb9 <sergiomartinez9mx@gmail.com>
2024-06-05 23:55:25 -0600

    datamart interfaseE

--- src/main/java/ProyectoDB/frontend/DataMartGUI.java
+++ src/main/java/ProyectoDB/frontend/DataMartGUI.java
@@ -10,26 +10,26 @@ public class DataMartGUI extends JFrame {
     private JPanel mainPanel;
     private JPanel imagePanel;
     private JPanel checkBoxPanel;
-    private JPanel textPanel;  // Panel for the JTextField
-    private JPanel datePanel;  // Panel for the first set of JComboBoxes
-    private JPanel datePanel2;  // New panel for the second set of JComboBoxes
+    private JPanel textPanel;
+    private JPanel datePanel;
+    private JPanel datePanel2;
     private JCheckBox checkBox1;
     private JCheckBox checkBox2;
     private JCheckBox checkBox3;
     private JTextField textField;
-    private JLabel textFieldLabel;  // New JLabel
-    private JComboBox<String> comboBox1;  // JComboBox for the first date
-    private JComboBox<String> comboBox2;  // JComboBox for the first date
-    private JComboBox<String> comboBox3;  // JComboBox for the first date
-    private JComboBox<String> comboBox4;  // New JComboBox for the second date
-    private JComboBox<String> comboBox5;  // New JComboBox for the second date
-    private JComboBox<String> comboBox6;  // New JComboBox for the second date
-    private JButton selectDateButton;  // JButton for the first date
-    private JButton selectDateButton2;  // New JButton for the second date
+    private JLabel textFieldLabel;
+    private JComboBox<String> comboBox1;
+    private JComboBox<String> comboBox2;
+    private JComboBox<String> comboBox3;
+    private JComboBox<String> comboBox4;
+    private JComboBox<String> comboBox5;
+    private JComboBox<String> comboBox6;
+    private JButton selectDateButton;
+    private JButton selectDateButton2;
 
     public DataMartGUI() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1000, 1000);
+        setSize(1050, 1000);
         setLayout(new BorderLayout());
 
         imageLabel = new JLabel();
@@ -37,15 +37,15 @@ public class DataMartGUI extends JFrame {
         imageLabel.setIcon(new ImageIcon(DataMartGUI.class.getResource("/images/flower.png")));
 
         checkBox1 = new JCheckBox("Resumen venta de productos");
-        checkBox1.setFont(new Font("Arial", Font.BOLD, 14));
+        checkBox1.setFont(new Font("Georgia", Font.BOLD, 14));
         checkBox2 = new JCheckBox("Resumen venta de farmacia");
-        checkBox2.setFont(new Font("Arial", Font.BOLD, 14));
+        checkBox2.setFont(new Font("Georgia", Font.BOLD, 14));
         checkBox3 = new JCheckBox("Desglose ventas por producto");
-        checkBox3.setFont(new Font("Arial", Font.BOLD, 14));
+        checkBox3.setFont(new Font("Georgia", Font.BOLD, 14));
 
-        textField = new JTextField(20);  // Increased the number of columns
-        textField.setFont(new Font("Arial", Font.PLAIN, 14));
-        textFieldLabel = new JLabel("Nombre del producto: ");  // Create the new JLabel
+        textField = new JTextField(20);
+        textField.setFont(new Font("Georgia", Font.PLAIN, 14));
+        textFieldLabel = new JLabel("Nombre del producto: ");
 
         String[] days = new String[31];
         for (int i = 0; i < 31; i++) {
@@ -59,13 +59,13 @@ public class DataMartGUI extends JFrame {
             years[i] = Integer.toString(i + 1945);
         }
 
-        comboBox1 = new JComboBox<>(days);  // Create the new JComboBox
-        comboBox2 = new JComboBox<>(months);  // Create the new JComboBox
-        comboBox3 = new JComboBox<>(years);  // Create the new JComboBox
+        comboBox1 = new JComboBox<>(days);
+        comboBox2 = new JComboBox<>(months);
+        comboBox3 = new JComboBox<>(years);
 
-        comboBox4 = new JComboBox<>(days);  // Create the new JComboBox for the second date
-        comboBox5 = new JComboBox<>(months);  // Create the new JComboBox for the second date
-        comboBox6 = new JComboBox<>(years);  // Create the new JComboBox for the second date
+        comboBox4 = new JComboBox<>(days);
+        comboBox5 = new JComboBox<>(months);
+        comboBox6 = new JComboBox<>(years);
 
         selectDateButton = new JButton("Fecha inicial ");
         selectDateButton.addActionListener(new ActionListener() {
@@ -91,34 +91,34 @@ public class DataMartGUI extends JFrame {
             }
         });
 
-        textPanel = new JPanel();  // Create the new panel
-        textPanel.add(textFieldLabel);  // Add the JLabel to the new panel
-        textPanel.add(textField);  // Add the JTextField to the new panel
+        textPanel = new JPanel();
+        textPanel.add(textFieldLabel);
+        textPanel.add(textField);
 
-        datePanel = new JPanel(new FlowLayout());  // Change to FlowLayout
-        datePanel.add(comboBox1);  // Add the JComboBox to the new panel
-        datePanel.add(comboBox2);  // Add the JComboBox to the new panel
-        datePanel.add(comboBox3);  // Add the JComboBox to the new panel
-        datePanel.add(selectDateButton);  // Add the JButton to the new panel
+        datePanel = new JPanel(new FlowLayout());
+        datePanel.add(comboBox1);
+        datePanel.add(comboBox2);
+        datePanel.add(comboBox3);
+        datePanel.add(selectDateButton);
 
-        datePanel2 = new JPanel(new FlowLayout());  // Create the new panel for the second date
-        datePanel2.add(comboBox4);  // Add the JComboBox to the new panel
-        datePanel2.add(comboBox5);  // Add the JComboBox to the new panel
-        datePanel2.add(comboBox6);  // Add the JComboBox to the new panel
-        datePanel2.add(selectDateButton2);  // Add the JButton to the new panel
+        datePanel2 = new JPanel(new FlowLayout());
+        datePanel2.add(comboBox4);
+        datePanel2.add(comboBox5);
+        datePanel2.add(comboBox6);
+        datePanel2.add(selectDateButton2);
 
         imagePanel = new JPanel();
         imagePanel.add(imageLabel);
-        imagePanel.setPreferredSize(new Dimension(300, 300));  // Set the preferred size
+        imagePanel.setPreferredSize(new Dimension(500, 500));
 
-        checkBoxPanel = new JPanel(new FlowLayout());  // Change to FlowLayout
+        checkBoxPanel = new JPanel(new FlowLayout());
         checkBoxPanel.add(checkBox1);
         checkBoxPanel.add(checkBox2);
         checkBoxPanel.add(checkBox3);
-        checkBoxPanel.add(Box.createRigidArea(new Dimension(10, 0)));  // Add horizontal space
-        checkBoxPanel.add(textPanel);  // Add the new panel to the checkBoxPanel
-        checkBoxPanel.add(datePanel);  // Add the new panel to the checkBoxPanel
-        checkBoxPanel.add(datePanel2);  // Add the new panel to the checkBoxPanel
+        checkBoxPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        checkBoxPanel.add(textPanel);
+        checkBoxPanel.add(datePanel);
+        checkBoxPanel.add(datePanel2);
 
         mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
@@ -132,15 +132,15 @@ public class DataMartGUI extends JFrame {
         setVisible(true);
     }
 
-    // Method to change the size of the image panel
+
     public void setImagePanelSize(int width, int height) {
         imagePanel.setPreferredSize(new Dimension(width, height));
-        imagePanel.revalidate();  // Update the panel
+        imagePanel.revalidate();
     }
 
-    // Method to change the font size of the text field
+
     public void setTextFieldFontSize(int size) {
-        textField.setFont(new Font("Arial", Font.PLAIN, size));
+        textField.setFont(new Font("Georgia", Font.PLAIN, size));
     }
 
     public static void main(String[] args) {
